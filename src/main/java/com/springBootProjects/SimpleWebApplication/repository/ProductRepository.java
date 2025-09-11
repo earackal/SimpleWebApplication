8873cce992b4137695ed184c45540672eedf9276
Emmanuel Arackal <emmanuelarackal@hotmail.com>
2025-09-11 09:30:11 +0200

    created repository class

---
+++ src/main/java/com/springBootProjects/SimpleWebApplication/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.springBootProjects.SimpleWebApplication.repository;
+
+import com.springBootProjects.SimpleWebApplication.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
