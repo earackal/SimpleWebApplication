f20b2200750688d48cc02a1f9a013751f21cea6c
Emmanuel Arackal <emmanuelarackal@hotmail.com>
2025-10-29 16:48:33 +0100

    added /product page to display product details

--- frontend/src/app/detail/product-view/product-view.ts
+++ frontend/src/app/detail/product-view/product-view.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../model/product';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-view',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-view.html',
   styleUrl: './product-view.css'
 })
 export class ProductView {
   @Input() product!: Product;
+  quantity = 1;
 }
